adb8506b001e465afc41fd6d4c534ef772f0ed98
matthcarson01 <matthcarson01@gmail.com>
2021-08-13 17:02:28 -0400

    Added initial style and added initial value string

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 function App() {
 
-  const [value, setValue] = useState('# Marked in the browser\n\nRendered by **marked**.');
+  const initialValue = "# This is a header\n\n## This is a sub-heading...\nThis is a [links](https://www.freecodecamp.org)\n\nThis is some inlinecode, `<div></div>`.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\n\n\nThis is a\n> Block Quotes!\n\n\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\nHere is a image (of me by the way)\n\n![freeCodeCamp Logo](https://media-exp1.licdn.com/dms/image/C5603AQFm4pQ9jyKgNw/profile-displayphoto-shrink_200_200/0/1517704748282?e=1634169600&v=beta&t=R1L3pcGKgoV7u9f32qvtW6694zyZoAmKJha_URzB5dk)";
+  const [value, setValue] = useState(initialValue);
   const markedValue = marked(value);
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -14,7 +15,7 @@ function App() {
     <main>
       <section>
         <textarea
-          id="preview"
+          id="editor"
           onChange={handleChange}
           value={value}
         ></textarea>
