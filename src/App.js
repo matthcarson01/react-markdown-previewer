20373351c8489884beb8211711fef8c4c4cc7914
matthcarson01 <matthcarson01@gmail.com>
2021-08-13 15:41:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import marked from 'marked';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <textarea id="preview"></textarea>
+      </section>
+      <section>
+        <div 
+          id="preview"
+          dangerouslySetInnerHTML={{__html: data}}
+        />
+      </section>
+    </main>
   );
 }
 
