414eab69736a51d9252ac73b19c4a99ac8dfc79e
matthcarson01 <matthcarson01@gmail.com>
2021-08-13 15:53:04 -0400

    added marked library and handleChange function and added value state. Converted value to markdown using marked library

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import marked from 'marked';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState('# Marked in the browser\n\nRendered by **marked**.');
+  const markedValue = marked(value);
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+  }
   return (
     <main>
       <section>
-        <textarea id="preview"></textarea>
+        <textarea
+          id="preview"
+          onChange={handleChange}
+          value={value}
+        ></textarea>
       </section>
       <section>
-        <div 
+        <div
           id="preview"
-          dangerouslySetInnerHTML={{__html: data}}
+          dangerouslySetInnerHTML={{ __html: markedValue }}
         />
       </section>
     </main>
